7af5f546388852d1619f5d0b7345f9df39682203
daxval <54683150+daxval@users.noreply.github.com>
2021-03-10 22:47:50 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function searchGoogleBooks(){
+function searchGoogleBooks() {
     var txtSearch = document.querySelector("#txtSearch")
-    if(txtSearch.value ==""){
+    if (txtSearch.value == "") {
         document.querySelector("#mainDiv").classList.add("hasError")
         document.querySelector("#mainDiv").innerHTML = "No search keyword provided..."
         return;
@@ -9,20 +9,20 @@ function searchGoogleBooks(){
     document.querySelector("#mainDiv").classList.remove("hasError")
 
     fetch('https://www.googleapis.com/books/v1/volumes?q=' + txtSearch.value + "&startIndex=0&maxResults=20")
-    .then(response => response.json())
-    .then(json => {
-        var list = `<div class="row col-8 center">found ${json.totalItems} books</div> <div class="row">`
-        for(let i = 0; i < json.items.length; i++){
-            console.log(json.items[i].volumeInfo.title)
-            var bookInfo = json.items[i].volumeInfo;
-            var template = `<ul class="col-sm-12 col-md-4 col-xl-3>`;
-            template += `<a class="list-group-item list-group-item-action active pretty-item" href="${json.items[i].selfLink}">${bookInfo.title}</a>`
-            template += `<li class="list-group-item">${bookInfo.authors}</li>`
-            template += `<li class="list-group-item">${bookInfo.publisher}, ${bookInfo.publishedDate}</li>`
-            template += `</ul>`
-            list += template;
-        }
-        list += '</div>'
-        document.getElementById("mainDiv").innerHTML = list;
-    });
+        .then(response => response.json())
+        .then(json => {
+            var list = `<div class="row col-8 center">found ${json.totalItems} books</div> <div class="row">`
+            for (let i = 0; i < json.items.length; i++) {
+                console.log(json.items[i].volumeInfo.title)
+                var bookInfo = json.items[i].volumeInfo;
+                var template = `<ul class="col-sm-12 col-md-4 col-xl-3>`;
+                template += `<a class="list-group-item list-group-item-action active pretty-item" href="${json.items[i].selfLink}">${bookInfo.title}</a>`
+                template += `<li class="list-group-item">${bookInfo.authors}</li>`
+                template += `<li class="list-group-item">${bookInfo.publisher}, ${bookInfo.publishedDate}</li>`
+                template += `</ul>`
+                list += template;
+            }
+            list += '</div>'
+            document.getElementById("mainDiv").innerHTML = list;
+        });
 }
